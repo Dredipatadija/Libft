97f0d2a1c967d545fb891ddb357bd084abcf91e8
Dredi <159581153+Dredipatadija@users.noreply.github.com>
2024-02-16 16:47:50 +0100

    Create ft_putstr_fd at home, review at 42

---
+++ ft_putstr_fd.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arenilla <arenilla@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/15 12:48:35 by arenilla          #+#    #+#             */
+/*   Updated: 2024/02/15 13:28:41 by arenilla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <fcntl.h>
+#include <unistd.h>
+#include "libft.h"
+
+void	ft_putstr_fd(char *s, int fd)
+{
+  size_t  i;
+
+  i = 0;
+  while (s[i] != '\0')
+  {
+    write(fd, &s[i], sizeof(char));
+    i++;
+  }
+}
+
+/*int	main(void)
+{
+	char	c;
+	int		fd;
+	
+	c = 'e';
+	fd = open("prueba_fd", O_RDWR);
+	ft_putchar_fd(c, fd);
+	close(fd);
+	return(0);
+}*/
