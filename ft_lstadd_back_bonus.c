730876d3e35432874b379ce7ddfb8ba20c6f4e3e
Dredi <159581153+Dredipatadija@users.noreply.github.com>
2024-02-20 20:02:04 +0100

    Update ft_lstadd_back_bonus.c at home

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -3,13 +3,13 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-  while (*lst)
-    {
-      
-	new->next = *lst;
-	*lst = new;
+	t_list	*lastnode;
+
+	lastnode = ft_lstlast(*lst);
+	lastnode->next = new;
+	new->next = NULL;
 }
-/*int	main(void)
+int	main(void)
 {
 	t_list	*node;
 	t_list	*new;
@@ -17,7 +17,7 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 	node = ft_lstnew("viejo nodo");
 	new = ft_lstnew("nuevo y primer nodo ya colocado");
 	printf("antes: %s\n", node->content);
-	ft_lstadd_front(&node, new);
+	ft_lstadd_back(&node, new);
 	printf("después: %s\n", node->content);
 	printf("después: %s\n", node->next->content);
 	return (0);
