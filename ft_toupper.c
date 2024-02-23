a294547ed27fe40e17b221fd3b4b464a0678d652
Andrea Renilla Martin <arenilla@c1r12s4.42madrid.com>
2024-02-23 10:33:11 +0100

    upload toupper with main

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,10 +6,11 @@
 /*   By: arenilla <arenilla@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 17:25:41 by arenilla          #+#    #+#             */
-/*   Updated: 2024/01/31 14:45:04 by arenilla         ###   ########.fr       */
+/*   Updated: 2024/02/23 10:32:47 by arenilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+//#include <ctype.h>
 #include "libft.h"
 
 int	ft_toupper(int c)
@@ -18,3 +19,15 @@ int	ft_toupper(int c)
 		c = c - 32;
 	return (c);
 }
+
+/*int	main(void)
+{
+	int	c;
+
+	c = 'r';
+	c = ft_toupper(c);
+	c = toupper(c);
+	write(1, &c, 1);
+	write(1, " ", 1);
+	return (0);
+}*/
