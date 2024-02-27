a23181a16f24081f2612acb409a501bb5a6586a0
Andrea Renilla Martin <arenilla@c1r16s4.42madrid.com>
2024-02-27 09:31:22 +0100

    upload strjoin with protection

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: arenilla <arenilla@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 09:14:19 by arenilla          #+#    #+#             */
-/*   Updated: 2024/02/23 08:20:30 by arenilla         ###   ########.fr       */
+/*   Updated: 2024/02/27 09:30:47 by arenilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,18 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	i = 0;
 	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
 	lenght = ft_strlen(s1) + ft_strlen(s2);
 	str = (char *)malloc(sizeof(char) * (lenght + 1));
 	if (!str)
 		return (0);
-	while (s1[i])
+	while (s1[i] != '\0')
 	{
 		str[i] = s1[i];
 		i++;
 	}
-	while (s2[j])
+	while (s2[j] != '\0')
 	{
 		str[i + j] = s2[j];
 		j++;
@@ -45,8 +47,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char const	*s1;
 	char const	*s2;
 
-	s1 = "hola, ";
-	s2 = "colegui";
+	s1 = "hola";
+	s2 = "";
 	printf("%s\n", ft_strjoin(s1, s2));
 	return (0);
 }*/
