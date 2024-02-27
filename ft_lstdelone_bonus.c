2b1ba7e1f25a1f49fc3a64e4be463eed8e98b9ff
Andrea Renilla Martin <arenilla@c1r16s4.42madrid.com>
2024-02-27 14:24:07 +0100

    upload lstdelone revie but without success at the main, fuck

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,17 +6,17 @@
 /*   By: arenilla <arenilla@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 12:49:37 by arenilla          #+#    #+#             */
-/*   Updated: 2024/02/23 08:15:16 by arenilla         ###   ########.fr       */
+/*   Updated: 2024/02/27 14:22:55 by arenilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
+#include <stdio.h>
 #include "libft.h"
 
-/*static void del(void *lst)
+static void ft_del(void *lst)
 {
 	free(lst);
-}*/
+}
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
@@ -24,13 +24,18 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *))
 		return ;
 	del(lst->content);
 	free(lst);
+	lst = 0;
 }
 
-/*int	main(void)
+int	main(void)
 {
-	t_list	*lst;
+	t_list	*node;
 
-	lst = ft_lstnew(ft_strdup("hola"));
-	ft_lstdelone(lst, del);
+	node = ft_lstnew(ft_strdup("hola"));
+	printf("antes contenido: %s\n", node->content);
+	printf("antes nodo: %p\n", *node);
+	ft_lstdelone(node, &ft_del);
+	printf("despues contenido: %s\n", node->content);
+	printf("despues nodo: %p\n", *node);
 	return (0);
-}*/
+}
