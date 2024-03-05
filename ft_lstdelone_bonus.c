570e07e555b978f3c647d331ba21eaab7d5a0a0c
Andrea Renilla Martin <arenilla@c2r5s6.42madrid.com>
2024-03-05 14:58:01 +0100

    lstdelone, main wrong, i can't, i want to cry

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,17 +6,17 @@
 /*   By: arenilla <arenilla@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 12:49:37 by arenilla          #+#    #+#             */
-/*   Updated: 2024/02/29 08:28:24 by arenilla         ###   ########.fr       */
+/*   Updated: 2024/03/05 14:57:09 by arenilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
+#include <stdio.h>
 #include "libft.h"
 
-/*static void ft_del(void *lst)
+static void del(void *lst)
 {
 	free(lst);
-}*/
+}
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
@@ -24,18 +24,22 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *))
 		return ;
 	del(lst->content);
 	free(lst);
-	lst = 0;
+	lst = NULL;
 }
 
-/*int	main(void)
+int	main(void)
 {
-	t_list	*node;
-
-	node = ft_lstnew(ft_strdup("hola"));
-	printf("antes: %s\n", node->content);
-	printf("%p\n",(void *)node);
-	ft_lstdelone(node, ft_del);
-	printf("despues: %s\n", node->content);
-	printf("%p\n", (void *)node);
-	return (0);
-}*/
+	t_list	*node1;
+	t_list	*node2;
+	t_list	*node3;
+	
+	node1 = ft_lstnew(ft_strdup("Hola"));
+	node2 = ft_lstnew(ft_strdup("borrado"));
+	node3 = ft_lstnew(ft_strdup("ultimo nodo, borrame"));
+	node1->next = node2;
+	node2->next = node3;
+	node3->next = NULL;
+	printf("sin borrar el ultimo nodo: %d\n", ft_lstsize(node1));
+	ft_lstdelone(node3, del);
+	printf("ultimo nodo borrado: %d\n", ft_lstsize(node1));
+}
