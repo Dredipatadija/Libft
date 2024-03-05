020f30c7da8df1ee3b6babe1e555e0a01f22ef8f
Andrea Renilla Martin <arenilla@c2r5s6.42madrid.com>
2024-03-05 13:49:56 +0100

    lstsize with free

--- ft_lstsize_bonus.c
+++ ft_lstsize_bonus.c
@@ -6,7 +6,7 @@
 /*   By: arenilla <arenilla@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 10:11:04 by arenilla          #+#    #+#             */
-/*   Updated: 2024/02/21 10:49:59 by arenilla         ###   ########.fr       */
+/*   Updated: 2024/03/05 13:49:07 by arenilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,18 +28,24 @@ int	ft_lstsize(t_list *lst)
 	return (i);
 }
 
-/*int main(void)
+/*static void del(void *lst)
+{
+	free(lst);
+}
+
+int main(void)
 {
 	t_list  *node1;
 	t_list  *node2;
 	t_list  *node3;
 	
-	node1 = ft_lstnew("Hola");
-	node2 = ft_lstnew("Hola");
-	node3 = ft_lstnew("Hola");
+	node1 = ft_lstnew(ft_strdup("Hola"));
+	node2 = ft_lstnew(ft_strdup("Hola"));
+	node3 = ft_lstnew(ft_strdup("Hola"));
 	node1->next = node2;
 	node2->next = node3;
 	node3->next = NULL;
 	printf("%d\n", ft_lstsize(node1));
+	ft_lstclear(&node1, del);
 	return 0;
 }*/
