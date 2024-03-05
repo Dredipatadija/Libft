2e4f21af749232bcda5a6030626fa05c3daf58e2
Andrea Renilla Martin <arenilla@c2r5s4.42madrid.com>
2024-03-05 11:54:23 +0100

    upload split with free

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: arenilla <arenilla@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 08:38:14 by arenilla          #+#    #+#             */
-/*   Updated: 2024/02/29 13:46:04 by arenilla         ###   ########.fr       */
+/*   Updated: 2024/03/05 11:53:03 by arenilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,27 +89,33 @@ char	**ft_split(char const *s, char c)
 		return ((void *)0);
 	string_array = (char **)malloc(sizeof(char *) * (ft_counter(s, c) + 1));
 	if (!string_array)
-		return (0);
+		return ((void *)0);
 	string_array = ft_loopsplit(string_array, s, c);
 	return (string_array);
 }
 
 /*int	main(void)
 {
-	char const	*s = NULL;
+	char const	*s = "hola";
 	char		c;
 	char		**result;
 	size_t		i;
 
 	i = 0;
-	c = ' ';
+	c = 'y';
 	result = ft_split(s, c);
-	if (!s)
+//Printf para s==NULL
+	if (!s && !result)
 		printf("%s\n",(char *)ft_split(s, c));
+//Printf para s==""
+	if (*s == 0 && result[i] == NULL)
+		printf("éxito");
+//Printf para s=="rellena"Si s==NULL o s=="", comentar el bucle. 
 	while (result[i] != 0)
 	{
 		printf("%s\n", result[i]);
 		i++;
 	}
+	free(result);
 	return (0);
 }*/
